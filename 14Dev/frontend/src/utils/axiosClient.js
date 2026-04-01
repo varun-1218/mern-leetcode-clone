633786c0ff907b1ac33c9f3b249f558427b982a0
Varun <svarun1218@gmail.com>
2026-04-01 17:03:03 +0530

    Use VITE_API_URL env variable for backend URL

--- 14Dev/frontend/src/utils/axiosClient.js
+++ 14Dev/frontend/src/utils/axiosClient.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 const axiosClient =  axios.create({
-    baseURL: 'http://localhost:3000',
+    // baseURL: 'http://localhost:3000',
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
